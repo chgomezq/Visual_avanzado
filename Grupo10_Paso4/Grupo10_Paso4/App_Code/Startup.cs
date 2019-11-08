3e2a609b5ad85ae61f7ed1223539012016b2786d
Cristiam Gomez <cristiam-gomez@hotmail.com>
2019-11-08 01:45:39 -0500

    Ver1.0

---
+++ Grupo10_Paso4/Grupo10_Paso4/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Grupo10_Paso4.Startup))]
+namespace Grupo10_Paso4
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
